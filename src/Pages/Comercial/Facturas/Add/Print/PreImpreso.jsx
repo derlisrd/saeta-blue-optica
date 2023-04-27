5ba848cf717cb56d6ea8e532a99e73b378f907d6
derlisrd <derlisruizdiaz@hotmail.com>
2023-04-27 16:42:19 -0400

    factura pre iumpresa

--- src/Pages/Comercial/Facturas/Add/Print/PreImpreso.jsx
+++ src/Pages/Comercial/Facturas/Add/Print/PreImpreso.jsx
@@ -1,29 +1,216 @@
-import {Dialog,DialogContent,DialogActions} from '@mui/material'
-import styles from './stylePreImpreso.module.css'
+import {Dialog,DialogContent,DialogActions, Button} from '@mui/material'
+import './style.css'
+import { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
+
 function PreImpreso() {
-    
-    const imprimir = ()=>{
-        printJS({ type: "html", printable: "print",
-        style:`#print{
-            margin:0 auto;
-            width: 220mm;
-        }`
-    });
-    }
+    const divRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => divRef.current,
+      });
     return (<Dialog open={true} onClose={()=>{}} fullScreen>
         <DialogContent>
-            <div id='print' className={styles.print}>
-                <table width='100%'>
+        
+            <div id='id_print_preimpreso' className='print_table_preimpreso' ref={divRef}>
+            <table className='table_inicial'  >
                     <tbody>
                         <tr>
-                            <td>hola</td>
+                            <td width='70%'>
+                                
+                            </td>
+                            <td align='center'> 
+                                <p>001-001- 000628</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_dos' >
+                    <tbody>
+                        <tr>
+                            <td width='20%'></td>
+                            <td width='60%'>
+                                <p>24-03-2022</p>
+                                <p>Ruiz Diaz Romero, Derlis Francisco</p>
+                                <p>_</p>
+                                <p>4937724-8</p>
+                            </td>
+                            <td> 
+                                <b>CONTADO</b>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_descripciones' border={0} >
+                    <tbody>{
+                        [0,1,2,3,4,5,7,8].map(e=>(
+                            <tr key={e}>
+                            <td width='7%'>0039</td>
+                            <td width='5%'>2</td>
+                            <td width='40%'>1.56 AR -5.00 A +4.00 CIL. -2.25 A -4.00</td>
+                            <td width='12%'>47.500</td>
+                            <td width='12%'>0</td>
+                            <td width='12%'>0</td>
+                            <td width='12%'>95.000</td>
+                        </tr>
+                        ))
+                     }
+                     <tr>
+                        <td colSpan={4}>
+
+                        </td>
+                        <td>47.500</td>
+                        <td>0</td>
+                        <td>95.000</td>
+                     </tr>
+                     <tr>
+                        <td colSpan={7}> NOVENTA Y CINCO MIL OCHOCIENTOS CATORCE </td>
+                     </tr>
+                    </tbody>
+                </table>
+                <table className='table_liquidacion'>
+                    <tbody>
+                        <tr>
+                            <td width='32%'></td>
+                            <td width='22%'>0</td>
+                            <td width='22%'>0</td>
+                            <td width='24%'>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_inicial'  >
+                    <tbody>
+                        <tr>
+                            <td width='70%'>
+                                
+                            </td>
+                            <td align='center'> 
+                                <p>001-001- 000628</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_dos' >
+                    <tbody>
+                        <tr>
+                            <td width='20%'></td>
+                            <td width='60%'>
+                                <p>24-03-2022</p>
+                                <p>Ruiz Diaz Romero, Derlis Francisco</p>
+                                <p>_</p>
+                                <p>4937724-8</p>
+                            </td>
+                            <td> 
+                                <b>CONTADO</b>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_descripciones' border={0} >
+                    <tbody>{
+                        [0,1,2,3,4,5,7,8].map(e=>(
+                            <tr key={e}>
+                            <td width='7%'>0039</td>
+                            <td width='5%'>2</td>
+                            <td width='40%'>1.56 AR -5.00 A +4.00 CIL. -2.25 A -4.00</td>
+                            <td width='12%'>47.500</td>
+                            <td width='12%'>0</td>
+                            <td width='12%'>0</td>
+                            <td width='12%'>95.000</td>
+                        </tr>
+                        ))
+                     }
+                     <tr>
+                        <td colSpan={4}>
+
+                        </td>
+                        <td>47.500</td>
+                        <td>0</td>
+                        <td>95.000</td>
+                     </tr>
+                     <tr>
+                        <td colSpan={7}> NOVENTA Y CINCO MIL OCHOCIENTOS CATORCE </td>
+                     </tr>
+                    </tbody>
+                </table>
+                <table className='table_liquidacion'>
+                    <tbody>
+                        <tr>
+                            <td width='32%'></td>
+                            <td width='22%'>0</td>
+                            <td width='22%'>0</td>
+                            <td width='24%'>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_inicial'  >
+                    <tbody>
+                        <tr>
+                            <td width='70%'>
+                                
+                            </td>
+                            <td align='center'> 
+                                <p>001-001- 000628</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_dos' >
+                    <tbody>
+                        <tr>
+                            <td width='20%'></td>
+                            <td width='60%'>
+                                <p>24-03-2022</p>
+                                <p>Ruiz Diaz Romero, Derlis Francisco</p>
+                                <p>_</p>
+                                <p>4937724-8</p>
+                            </td>
+                            <td> 
+                                <b>CONTADO</b>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table_descripciones' border={0} >
+                    <tbody>{
+                        [0,1,2,3,4,5,7,8].map(e=>(
+                            <tr key={e}>
+                            <td width='7%'>0039</td>
+                            <td width='5%'>2</td>
+                            <td width='40%'>1.56 AR -5.00 A +4.00 CIL. -2.25 A -4.00</td>
+                            <td width='12%'>47.500</td>
+                            <td width='12%'>0</td>
+                            <td width='12%'>0</td>
+                            <td width='12%'>95.000</td>
+                        </tr>
+                        ))
+                     }
+                     <tr>
+                        <td colSpan={4}>
+
+                        </td>
+                        <td>47.500</td>
+                        <td>0</td>
+                        <td>95.000</td>
+                     </tr>
+                     <tr>
+                        <td colSpan={7}> NOVENTA Y CINCO MIL OCHOCIENTOS CATORCE </td>
+                     </tr>
+                    </tbody>
+                </table>
+                <table className='table_liquidacion'>
+                    <tbody>
+                        <tr>
+                            <td width='32%'></td>
+                            <td width='22%'>0</td>
+                            <td width='22%'>0</td>
+                            <td width='24%'>0</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </DialogContent>
         <DialogActions>
-            <button onClick={imprimir}>print</button>
+            <Button variant='contained' size='large' onClick={handlePrint} >print</Button>
         </DialogActions>
     </Dialog>);
 }
--- src/Pages/Comercial/Pedidos/Components/Receta.jsx
+++ src/Pages/Comercial/Pedidos/Components/Receta.jsx
@@ -1,14 +1,10 @@
-import { Dialog, DialogTitle,DialogContent,  Grid,  DialogActions, Button, IconButton, Typography } from "@mui/material";
-import { usePedidos } from "./PedidosProvider";
-import styles from './styles.module.css'
+import { Fragment } from "react";
+import { usePedidos } from "../PedidosProvider";
 import { useState } from "react";
-import { Icon } from "@iconify/react";
-import InputNumerico from "./Components/InputNumerico";
+import InputNumerico from "./InputNumerico";
 
-function DialogSelectDepositoStock() {
-
-    const {setDialogs,dialogs,formDepositoStock,setearFactura,factura,selectProduct} = usePedidos()
-    
+function Receta() {
+    const {setearFactura,factura}  = usePedidos()
     const initialParam = {
         lejos_derecho_esferico:'0',
         lejos_derecho_cilindrico:'0',
@@ -23,15 +19,8 @@ function DialogSelectDepositoStock() {
         cerca_izquierdo_esferico:'0',
         cerca_eje_derecho:'0',
         cerca_eje_izquierdo:'0',
-
         adicion_izquierdo:'0',
-        adicion_derecho:'0',
-
-        dnp_izquierdo:'0',
-        dnp_derecho:'0',
-
-        altura_derecho:'0',
-        altura_izquierdo:'0'
+        adicion_derecho:'0'
     }
    const [param,setParam] = useState(initialParam)
 
@@ -92,12 +81,7 @@ function DialogSelectDepositoStock() {
         setParam({...param, [name]:value})
     }
 
-    const insertarReceta = ()=>{
-        let new_fact = {...factura}
-        new_fact.receta = {...param}
-        setearFactura(new_fact)
-        close()
-    }
+    
 
     const key = e=>{
         const {value,name} = e.target
@@ -127,15 +111,8 @@ function DialogSelectDepositoStock() {
         }
     }
 
-    const close = ()=>{ setDialogs({...dialogs,select_deposito_stock:false}); }
-
-
-
-    return ( <Dialog open={dialogs.select_deposito_stock} fullWidth maxWidth="lg" onClose={close} >
-        <DialogTitle><IconButton onClick={close} ><Icon icon="ic:twotone-close" /> </IconButton> {selectProduct?.codigo_producto} - { selectProduct?.nombre_producto}  </DialogTitle>
-        <DialogContent>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
+    return (<Fragment>
+        <Grid item xs={12} sm={6}>
                     <Grid container alignItems='center' spacing={1} sx={{ border:'1px solid silver',padding:1,borderRadius:1 }}>
                         <Grid item xs={12}><Typography variant="button">LEJOS</Typography></Grid>
                         <Grid item xs={12} sm={3}><Typography variant="overline">DERECHO:</Typography></Grid>
@@ -169,53 +146,7 @@ function DialogSelectDepositoStock() {
                         
                     </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Grid container alignItems='center' spacing={1} sx={{ border:'1px solid silver',padding:1,borderRadius:1 }}>
-                        <Grid item xs={12} sm={4}><Typography variant="button">DNP</Typography></Grid>
-                        <Grid item xs={12} sm={4}><InputNumerico name="dnp_derecho" onChange={change} value={param.dnp_derecho} fullWidth label='DNP DERECHO' /> </Grid>
-                        <Grid item xs={12} sm={4}><InputNumerico name="dnp_izquierdo" onChange={change} value={param.dnp_izquierdo} fullWidth label='DNP IZQUIERDO' /> </Grid>
-                        <Grid item xs={12} sm={4}><Typography variant="button">ALTURA</Typography></Grid>
-                        <Grid item xs={12} sm={4}><InputNumerico name="altura_derecho" onChange={change} value={param.altura_derecho} fullWidth label='Altura derecha' /> </Grid>
-                        <Grid item xs={12} sm={4}><InputNumerico name="altura_izquierdo" onChange={change} value={param.altura_izquierdo} fullWidth label='Altura izquierda' /> </Grid>
-                    </Grid>
-                </Grid>
-                
-
-
-                <Grid item xs={12}>
-                   <Typography variant="button">STOCK DISPONIBLE</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                <table className={styles.table} width="100%" border={1}>
-                    <tbody>
-                        <tr>
-                            <th width='25%'>Esférico</th>
-                            <th width='25%'>Cilindrico</th>
-                            <th width='25%'>Eje</th>
-                            <th width='25%'>Stock disponible</th>
-                        </tr>
-                        
-                        {
-                            formDepositoStock.map((e,i)=>(
-                            <tr key={i} className={styles.items_stock}>
-                                <td width='25%'>{e.graduacion_esferico}</td>
-                                <td width='25%'>{e.graduacion_cilindrico}</td>
-                                <td width='25%'>{e.eje}</td>
-                                <td width='25%'>{e.stock_producto_deposito}</td>
-                            </tr>
-                            ))
-                        }
-
-                    </tbody>
-                </table>
-                </Grid>
-            </Grid>
-        </DialogContent>
-        <DialogActions>
-            <Button variant="contained" onClick={insertarReceta}>Insertar</Button>
-            <Button variant="outlined" onClick={close}>Cerrar</Button>
-        </DialogActions>
-    </Dialog> );
+    </Fragment>  );
 }
 
-export default DialogSelectDepositoStock;
+export default Receta;

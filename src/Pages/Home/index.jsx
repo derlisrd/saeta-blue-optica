9bef838e9da47dbc94f6b2aacf09d7df9a2390be
derlisrd <derlisruizdiaz@hotmail.com>
2023-06-08 12:36:54 -0400

    home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,9 +5,9 @@ import { Box,  Typography } from "@mui/material";
 function Home() {
     return (<Box sx={{ display:'flex',alignItems:'center',justifyContent:'center',gap:'8px',flexDirection:'column' }}>
 
-                    <span role="img" style={{ fontSize:'10rem' }} >🦁</span>
+                    <span role="img" style={{ fontSize:'10rem' }} ></span>
                 <Typography variant="h3">
-                    LIONS
+                    ZOOM OPTICAL
                 </Typography>
                 <Typography variant="h3">
                     Sistema de Gestion Administrativo
